936049f5144f6f3d76c0f926fb5bb85f96f894f9
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-04-30 19:54:59 -0500

    Blank install from Blacklight template installer

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'c84c6dfda54bd65ca877a7d5885a1f67a8837902a2c2ea7f033525906601f3a0646979f46638110b18203f90592c3b44e58a3098f0a51026c38b7d7886f643dd';
