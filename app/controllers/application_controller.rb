4b078922fdb6a7e607c1bd02e294fdd9ea11ee95
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-04-30 21:00:35 -0500

    tweaking Blacklight require_dependency calls & adding role_map_test.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
-require_dependency( 'vendor/plugins/blacklight/app/controllers/application_controller.rb')
+# require_dependency( 'vendor/plugins/blacklight/app/controllers/application_controller.rb')
+require_dependency File.join(File.dirname(__FILE__), '..','..','vendor','plugins','blacklight','app','controllers','application_controller.rb')
+
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
+require File.join(File.dirname(__FILE__), '..','..','vendor','plugins','blacklight','app','helpers','application_helper.rb')
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 end
