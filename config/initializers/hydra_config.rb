f525d3e129ee7ca308a79a11d657f9f8b48c6a0a
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-08 18:19:10 -0500

    tweaking hydra_config to work with plugin-testing rake task

--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -1,3 +1,4 @@
+require "hydra"
 # The following lines determine which user attributes your hydrangea app will use
 # This configuration allows you to use the out of the box ActiveRecord associations between users and user_attributes
 # It also allows you to specify your own user attributes
@@ -8,16 +9,19 @@
 #
 # NOTE: For your development environment, also specify the module in lib/user_attributes_loader.rb
 User.send(:include, Hydra::GenericUserAttributes)
+# 
 
-Hydra.configure(:shared) do |config|
+if Hydra.respond_to?(:configure)
+  Hydra.configure(:shared) do |config|
   
-  config[:file_asset_types] = {
-    :default => FileAsset, 
-    :extension_mappings => {
-      AudioAsset => [".wav", ".mp3", ".aiff"] ,
-      VideoAsset => [".mov", ".flv", ".mp4", ".m4v"] ,
-      ImageAsset => [".jpeg", ".jpg", ".gif", ".png"] 
+    config[:file_asset_types] = {
+      :default => FileAsset, 
+      :extension_mappings => {
+        AudioAsset => [".wav", ".mp3", ".aiff"] ,
+        VideoAsset => [".mov", ".flv", ".mp4", ".m4v"] ,
+        ImageAsset => [".jpeg", ".jpg", ".gif", ".png"] 
+      }
     }
-  }
 
+  end
 end
