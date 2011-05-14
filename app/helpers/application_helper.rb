89a3b6601d315b3e30c552cae2c42367ef48745f
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-14 15:27:12 -0500

    replacing require_dependency with require_plugin_dependency in host application_controller and application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
+require_plugin_dependency 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 end
