c9050d628c706e2675bf87d5314fa1ddceda1ec7
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-19 16:12:08 -0500

    reverting to blacklight's routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,45 @@
 ActionController::Routing::Routes.draw do |map|
-     map.resources :file_assets
-          
-     # This creates routes named asset_file_assets, new_asset_file_asset, etc.
-     # The routes map to the :file_assets controller with the param :container_id 
-     # which is then used within the controller to deal with parent - child relationships 
-     # between Assets and the FileAssets inside them.
-     map.resources :file_assets, :path_prefix => '/assets/:container_id', :name_prefix => "asset_"
-     
-     map.resources :assets do |assets|
-       assets.resources :downloads, :only=>[:index]
-       assets.resources :contributors, :only=>[:new,:create]
-       assets.resources :grants, :only=>[:new,:create]       
-       assets.resources :permissions
-    end
-    
-    map.edit_catalog 'catalog/:id/edit', :controller=>:catalog, :action=>:edit
-    map.delete_catalog "catalog/:id/delete", :controller=>:catalog, :action=>:delete
-    map.about 'about', :controller => 'catalog', :action => 'about'
-    
-    map.logged_out 'logged_out', :controller => 'user_sessions', :action => 'logged_out'
-    map.superuser 'superuser', :controller => 'user_sessions', :action => 'superuser'
-    
-    map.resources :get, :only=>:show 
-    
-    # this is to remove documents from SOLR but not from Fedora.
-    map.withdraw "withdraw", :controller => "assets", :action => :withdraw 
-    
-    map.asset_contributor 'assets/:asset_id/contributors/:contributor_type/:index', :controller=>:contributors, :action=>:show, :conditions => { :method => :get }
-    map.connect 'assets/:asset_id/contributors/:contributor_type/:index', :controller=>:contributors, :action=>:destroy, :conditions => { :method => :delete }
-    
-    map.asset_grant 'assets/:asset_id/grants/:grant_type/:index', :controller=>:grants, :action=>:show, :conditions => { :method => :get }
-    map.connect 'assets/:asset_id/grants/:grant_type/:index', :controller=>:grants, :action=>:destroy, :conditions => { :method => :delete }
-    
-    # Allow updates to assets/:asset_id/permissions (no :id necessary)
-    map.update_group_permissions 'assets/:asset_id/permissions', :controller=>:permissions, :action=>:update
-    
-    map.generic_content_object 'generic_contents_object/content/:container_id', :controller=>:generic_content_objects, :action=>:create, :conditions => {:method => :post}
+  Blacklight::Routes.build map
+
+  # The priority is based upon order of creation: first created -> highest priority.
+
+  # Sample of regular route:
+  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   map.resources :products
+
+  # Sample resource route with options:
+  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+
+  # Sample resource route with sub-resources:
+  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+  
+  # Sample resource route with more complex sub-resources
+  #   map.resources :products do |products|
+  #     products.resources :comments
+  #     products.resources :sales, :collection => { :recent => :get }
+  #   end
+
+  # Sample resource route within a namespace:
+  #   map.namespace :admin do |admin|
+  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
+  #     admin.resources :products
+  #   end
+
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  # map.root :controller => "welcome"
+
+  # See how all your routes lay out with "rake routes"
+
+  # Install the default routes as the lowest priority.
+  # Note: These default routes make all actions in every controller accessible via GET requests. You should
+  # consider removing or commenting them out if you're using named routes and resources.
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
 end
