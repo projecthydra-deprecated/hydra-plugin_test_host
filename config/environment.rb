56f225b24205b156bcdc21c419bdc55de5cf2c33
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-19 15:30:37 -0500

    Setting config/environment.rb to load plugins in correct order

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,8 @@ RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require File.join(File.dirname(__FILE__), '../vendor/plugins/blacklight/vendor/plugins/engines/boot')
+require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
+# require File.join(File.dirname(__FILE__), '../vendor/plugins/blacklight/vendor/plugins/engines/boot')
 
 Rails::Initializer.run do |config|
   config.plugin_paths += ["#{RAILS_ROOT}/vendor/plugins/blacklight/vendor/plugins"]
@@ -26,7 +27,7 @@ Rails::Initializer.run do |config|
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
+  config.plugins = %W(engines blacklight hydra-head white_list)
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
