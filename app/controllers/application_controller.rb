faa8a773f68cb57a5edd1f1a2e06d27819cfde18
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-27 16:33:35 -0500

    disabling protect_from_forgery for now -- was breaking file uploads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require_plugin_dependency( 'vendor/plugins/blacklight/app/controllers/applicatio
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  # protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
